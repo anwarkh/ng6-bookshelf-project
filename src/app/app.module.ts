05dacbd1ed3065a8b62438b1710f9b8e1db7bea2
akharrat <akharrat@galerieslafayette.com>
2018-08-23 21:31:09 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {PagerComponent} from "./pager/pager.component";
+import {LibraryComponent} from "./library/library.component";
+import {SearchComponent} from "./search/search.component";
+import {BookListComponent} from "./book-list/book-list.component";
+import {BookComponent} from "./book/book.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', redirectTo: 'search', pathMatch: 'full'},
+  { path: "search", component: SearchComponent },
+  { path: 'library', component: BookListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookComponent,
+    BookListComponent,
+    SearchComponent,
+    LibraryComponent,
+    PagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
